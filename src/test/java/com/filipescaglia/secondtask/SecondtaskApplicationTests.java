d906cff255fa4b6492b8e6517d3d314411e1c404
Filipe Scaglia <filipecscaglia@gmail.com>
2021-01-17 18:16:40 -0300

    Project created

---
+++ src/test/java/com/filipescaglia/secondtask/SecondtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipescaglia.secondtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
