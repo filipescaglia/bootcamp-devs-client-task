20d5b920bfa1d5d81ee05534fa1a07a5db4dba3d
Filipe Scaglia <filipecscaglia@gmail.com>
2021-01-18 22:40:50 -0300

    Resource exceptions

--- src/main/java/com/filipescaglia/secondtask/resources/ClientResource.java
+++ src/main/java/com/filipescaglia/secondtask/resources/ClientResource.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,5 +41,10 @@ public class ClientResource {
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+	
+	/*
+	 * @DeleteMapping public ResponseEntity<ClientDTO> delete(@PathVariable Long id)
+	 * { service.delete(id); return ResponseEntity.noContent().build(); }
+	 */
 
 }
